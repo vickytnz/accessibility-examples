7887f664c004ba654a11b348074eb0f8ec14eb5d
vickytnz <vicky.teinaki@gmail.com>
2024-06-01 17:27:19 +0100

    Big changes to layouts
    * Restructured pages to all have an introduction page
    * Created new place to put all reference links
    * Tidied up home page to be cleaner
    * Added menu
    * Added Contribute and Using these examples pages

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -2,4 +2,20 @@ module.exports = {
 
   // Insert values here
 
+  //list of all references as used across the examples
+ /* "reference" : {
+   'legends': {
+     text: "Making legends and labels headings on GOV.UK",
+     href: "https://design-system.service.gov.uk/get-started/labels-legends-headings/"
+   },
+   'complex-questions' : {
+     text: "Find out about complex question pages on GOV.UK",
+     href: "https://design-system.service.gov.uk/patterns/question-pages/#asking-complex-questions-without-using-hint-text"
+   },
+   'question-pages' : {
+     text: "Find out about question pages on GOV.UK",
+     href: "https://design-system.service.gov.uk/patterns/question-pages/"
+   } */ 
+   // end of references
+ }
 }
